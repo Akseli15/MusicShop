e408d530d3f72dbb3a7830b81bc4820bfcc5a34d
Createl15 <zlata2003@inbox.ru>
2023-06-08 22:07:55 +0300

    Update repositories

--- zlatik/src/main/java/com/example/zlatik/repository/ArtistRepository.java
+++ zlatik/src/main/java/com/example/zlatik/repository/ArtistRepository.java
@@ -1,7 +1,6 @@
 package com.example.zlatik.repository;
 
 import com.example.zlatik.entity.Artist;
-import com.example.zlatik.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
--- zlatik/src/main/java/com/example/zlatik/repository/BandRepository.java
+++ zlatik/src/main/java/com/example/zlatik/repository/BandRepository.java
@@ -1,6 +1,5 @@
 package com.example.zlatik.repository;
 
-import com.example.zlatik.entity.Artist;
 import com.example.zlatik.entity.Band;
 import org.springframework.data.jpa.repository.JpaRepository;
 
